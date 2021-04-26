fb77a05e91b56a8e958707b1b18e9e6689974448
Rio <riokimle123@gmail.com>
2021-04-26 14:26:17 -0700

    I did the top Nav

---
+++ lets-eat/src/landingpage/LandingPage.js
@@ -0,0 +1,12 @@
+import React from "react";
+import TopNav from "./TopNav/TopNav";
+
+function LandingPage() {
+  return (
+    <div>
+      <TopNav />
+    </div>
+  );
+}
+
+export default LandingPage;
