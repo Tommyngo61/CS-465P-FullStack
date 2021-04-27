833fa4b6504649308f74a86fafa6ab3e95cb512e
Rio <riokimle123@gmail.com>
2021-04-26 18:58:18 -0700

    I added the search stuff I needed now I will do api

--- lets-eat/src/landingpage/LandingPage.js
+++ lets-eat/src/landingpage/LandingPage.js
@@ -1,10 +1,12 @@
 import React from "react";
+import SearchBar from "./SearchBar/SearchBar";
 import TopNav from "./TopNav/TopNav";
 
 function LandingPage() {
   return (
     <div>
       <TopNav />
+      <SearchBar />
     </div>
   );
 }
