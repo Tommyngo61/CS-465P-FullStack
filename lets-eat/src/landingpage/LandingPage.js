87594d17261c2f607efcb58dd262d75bf95fe426
Tommy <tngo@pdx.edu>
2021-04-27 20:08:00 -0700

    Added text to the home page

--- lets-eat/src/landingpage/LandingPage.js
+++ lets-eat/src/landingpage/LandingPage.js
@@ -1,11 +1,17 @@
 import React from "react";
 import SearchBar from "./SearchBar/SearchBar";
 import TopNav from "./TopNav/TopNav";
+import "./LandingPage.css";
+//import { Container, Row, Col, Button } from "react-bootstrap";
+//import Jumbotron from "react-bootstrap/Jumbotron";
 
 function LandingPage() {
   return (
     <div>
       <TopNav />
+      <div className="homeCard">
+        <h1>Foodie Home Page</h1>
+      </div>
       <SearchBar />
     </div>
   );
