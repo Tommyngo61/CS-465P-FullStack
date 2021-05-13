87a9103b30fe49cd06cf99284fc67899947cde41
Tommy <tngo@pdx.edu>
2021-05-12 22:25:08 -0700

    Added stuff to search results

--- lets-eat/client/src/Search/Search.js
+++ lets-eat/client/src/Search/Search.js
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from "react";
+import { Container, Row, Col, Button } from "react-bootstrap";
 import TopNav from "../Landingpage/TopNav/TopNav";
 import { useLocation } from "react-router-dom";
+import ReactStars from "react-rating-stars-component";
 import axios from "axios";
 function Search() {
   let location1 = useLocation();
@@ -27,17 +29,62 @@ function Search() {
   return (
     <>
       <TopNav />
-      <h1>
-        {term} {location}
-      </h1>
-
-      {/* <ul>
-        {places.map((place) => {
-          return <li key={place.id}>{place.name}</li>;
-        })}
-      </ul> */}
+      <Container fluid>
+        <Row>
+          <Col>
+            <h1>Restaurant Name</h1>
+          </Col>
+        </Row>
+        <Row className="justify-content-md-center">
+          <Col xs={{ span: 3, offset: 1 }}>
+            <img src="https://via.placeholder.com/300" alt="hi"></img>
+          </Col>
+          <Col>
+            <ReactStars
+              count={5}
+              value={4}
+              size={30}
+              edit={false}
+              activeColor="#ffd700"
+            />
+            <h2>Description</h2>
+            <span>
+              Mauris laoreet congue luctus. Ut condimentum magna id risus
+              iaculis, ac mollis turpis ultrices. Sed facilisis dui neque,
+              malesuada dictum enim malesuada non. Cras congue auctor libero et
+              imperdiet. Vestibulum interdum arcu est. Aliquam ultricies
+              accumsan arcu id hendrerit. Fusce cursus, urna vel pulvinar
+              ullamcorper, augue lorem varius diam, ut varius erat sapien at
+              odio. Sed bibendum ante et ullamcorper hendrerit. Vestibulum
+              rhoncus est eu enim pharetra fringilla. Cras ultricies neque ac
+              augue fermentum, eget sodales erat feugiat. Quisque aliquet
+              finibus arcu, quis aliquet erat pharetra non. Nunc mattis metus
+            </span>
+          </Col>
+          <Col></Col>
+        </Row>
+        <Row className="mt-5 justify-content-md-center">
+          <Col xs={{ offset: 6 }}>
+            <Button variant="primary" size="lg" type="submit">
+              Reroll
+            </Button>
+          </Col>
+        </Row>
+      </Container>
     </>
   );
 }
+
+/*
+  <h1>
+        {term} {location}
+      </h1>
+
+      <ul>
+        {places.map((place) => {
+          return <li key={place.id}>{place.name}</li>;
+        })}
+      </ul> 
+*/
 
 export default Search;
